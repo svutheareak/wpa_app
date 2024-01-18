fa4ecb738fbb5c5340ba015cf49f7d8decbfa5a6
Sam Vutheareak <sam.vutheareak@DGB-STHEAREAK.local>
2024-01-18 16:49:43 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      // theme: ThemeData(
+      //   theme: ThemeData.dark(),
+      //   useMaterial3: true,
+      // ),
+       theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      // backgroundColor: Colors.black,
+      // appBar: AppBar(),
+      body:  SafeArea(bottom: false,child: WebView(
+        initialUrl: "https://youtube.com/",
+        javascriptMode: JavascriptMode.unrestricted,
+      ))
+    );
+  }
+}
